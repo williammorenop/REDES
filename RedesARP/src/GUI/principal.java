11c1c1bb0ea82cce1430e19e214777931f9428ec
William Moreno <williammorenoprieto@gmail.com>
2017-09-02 16:35:45 -0500

    GUI otra vez

--- RedesARP/src/GUI/principal.java
+++ RedesARP/src/GUI/principal.java
@@ -11,27 +11,41 @@ import javax.swing.JComboBox;
 import javax.swing.JToggleButton;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.table.DefaultTableModel;
+import javax.swing.JProgressBar;
 
 public class principal {
 
 	private JFrame frame;
 	private JScrollPane scrollPane;
-	private JTextField textField;
 	private JTextField textField_2;
 	private JTextField textField_3;
-	private JTextField textField_4;
-	private JTextField textField_5;
-	private JTextField textField_6;
+	private JTextField hardtype;
+	private JTextField macori;
+	private JTextField iporigi;
 	private JTextField textField_16;
-	private JTextField textField_1;
-	private JTextField textField_7;
-	private JTextField textField_8;
-	private JTextField textField_9;
-	private JTextField textField_10;
-	private JTextField textField_11;
-	private JTextField textField_12;
-	private JTextField textField_13;
-	private JTextField textField_14;
+	private JTextField reci1;
+	private JTextField reci2;
+	private JTextField reci3;
+	private JTextField reci4;
+	private JTextField reci5;
+	private JTextField reci6;
+	private JTextField reci7;
+	private JTextField reci8;
+	private JTextField reci9;
+	private JLabel lblNewLabel;
+	private JLabel lblNewLabel_1;
+	private JLabel lblOpercacion;
+	private JLabel lblHardware;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JProgressBar progressBar43;
 
 	/**
 	 * Launch the application.
@@ -78,33 +92,35 @@ public class principal {
 		ipdesti.setBounds(885, 308, 118, 32);
 		frame.getContentPane().add(ipdesti);
 		
-		JLabel lblAsdasd = new JLabel("mac des");
-		lblAsdasd.setBounds(739, 277, 92, 26);
+		JLabel lblAsdasd = new JLabel("MAC destino");
+		lblAsdasd.setBounds(741, 277, 136, 26);
 		frame.getContentPane().add(lblAsdasd);
 		
-		JLabel lblMacOrigen = new JLabel("mac origen");
-		lblMacOrigen.setBounds(429, 277, 92, 26);
+		JLabel lblMacOrigen = new JLabel("MAC origen");
+		lblMacOrigen.setBounds(418, 277, 152, 26);
 		frame.getContentPane().add(lblMacOrigen);
 		
-		JLabel lblIpDes = new JLabel("ip des");
-		lblIpDes.setBounds(898, 277, 92, 26);
+		JLabel lblIpDes = new JLabel("IP destino");
+		lblIpDes.setBounds(885, 277, 105, 26);
 		frame.getContentPane().add(lblIpDes);
 		
-		JLabel lblIpOri = new JLabel("ip ori");
-		lblIpOri.setBounds(608, 277, 92, 26);
+		JLabel lblIpOri = new JLabel("IP origen");
+		lblIpOri.setBounds(591, 277, 92, 26);
 		frame.getContentPane().add(lblIpOri);
 		
 		scrollPane = new JScrollPane();
-		scrollPane.setBounds(336, 21, 418, 216);
+		scrollPane.setBounds(582, 52, 418, 216);
 		frame.getContentPane().add(scrollPane);
 		
 		JTable tablaipmac = new JTable();
+		tablaipmac.setModel(new DefaultTableModel(
+			new Object[][] {
+			},
+			new String[] {
+				"IP", "MAC"
+			}
+		));
 		scrollPane.setViewportView(tablaipmac);
-		
-		textField = new JTextField();
-		textField.setColumns(10);
-		textField.setBounds(292, 349, 105, 32);
-		frame.getContentPane().add(textField);
 		
 		textField_2 = new JTextField();
 		textField_2.setEditable(false);
@@ -121,35 +137,35 @@ public class principal {
 		textField_3.setBounds(130, 308, 56, 32);
 		frame.getContentPane().add(textField_3);
 		
-		textField_4 = new JTextField();
-		textField_4.setColumns(10);
-		textField_4.setBounds(21, 308, 105, 32);
-		frame.getContentPane().add(textField_4);
+		hardtype = new JTextField();
+		hardtype.setColumns(10);
+		hardtype.setBounds(21, 308, 105, 32);
+		frame.getContentPane().add(hardtype);
 		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Otro"}));
-		comboBox_1.setBounds(292, 308, 105, 32);
-		frame.getContentPane().add(comboBox_1);
+		JComboBox seloperacion = new JComboBox();
+		seloperacion.setModel(new DefaultComboBoxModel(new String[] {"Request", "Reply"}));
+		seloperacion.setBounds(292, 308, 105, 32);
+		frame.getContentPane().add(seloperacion);
 		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Otro"}));
-		comboBox_2.setBounds(399, 308, 179, 32);
-		frame.getContentPane().add(comboBox_2);
+		JComboBox selecmacori = new JComboBox();
+		selecmacori.setModel(new DefaultComboBoxModel(new String[] {"Otro"}));
+		selecmacori.setBounds(399, 308, 179, 32);
+		frame.getContentPane().add(selecmacori);
 		
-		textField_5 = new JTextField();
-		textField_5.setColumns(10);
-		textField_5.setBounds(399, 349, 179, 32);
-		frame.getContentPane().add(textField_5);
+		macori = new JTextField();
+		macori.setColumns(10);
+		macori.setBounds(399, 349, 179, 32);
+		frame.getContentPane().add(macori);
 		
-		JComboBox comboBox_3 = new JComboBox();
-		comboBox_3.setModel(new DefaultComboBoxModel(new String[] {"Otro"}));
-		comboBox_3.setBounds(582, 308, 118, 32);
-		frame.getContentPane().add(comboBox_3);
+		JComboBox selectipori = new JComboBox();
+		selectipori.setModel(new DefaultComboBoxModel(new String[] {"Otro"}));
+		selectipori.setBounds(582, 308, 118, 32);
+		frame.getContentPane().add(selectipori);
 		
-		textField_6 = new JTextField();
-		textField_6.setColumns(10);
-		textField_6.setBounds(582, 349, 118, 32);
-		frame.getContentPane().add(textField_6);
+		iporigi = new JTextField();
+		iporigi.setColumns(10);
+		iporigi.setBounds(582, 349, 118, 32);
+		frame.getContentPane().add(iporigi);
 		
 		textField_16 = new JTextField();
 		textField_16.setEditable(false);
@@ -159,67 +175,119 @@ public class principal {
 		textField_16.setBounds(242, 308, 46, 32);
 		frame.getContentPane().add(textField_16);
 		
-		textField_1 = new JTextField();
-		textField_1.setEnabled(false);
-		textField_1.setColumns(10);
-		textField_1.setBounds(21, 460, 105, 32);
-		frame.getContentPane().add(textField_1);
+		reci1 = new JTextField();
+		reci1.setEnabled(false);
+		reci1.setColumns(10);
+		reci1.setBounds(21, 460, 105, 32);
+		frame.getContentPane().add(reci1);
 		
-		textField_7 = new JTextField();
-		textField_7.setText("2048");
-		textField_7.setEditable(false);
-		textField_7.setColumns(10);
-		textField_7.setBounds(130, 460, 56, 32);
-		frame.getContentPane().add(textField_7);
+		reci2 = new JTextField();
+		reci2.setText("2048");
+		reci2.setEditable(false);
+		reci2.setColumns(10);
+		reci2.setBounds(130, 460, 56, 32);
+		frame.getContentPane().add(reci2);
 		
-		textField_8 = new JTextField();
-		textField_8.setToolTipText("");
-		textField_8.setText("6");
-		textField_8.setEditable(false);
-		textField_8.setColumns(10);
-		textField_8.setBounds(192, 460, 46, 32);
-		frame.getContentPane().add(textField_8);
+		reci3 = new JTextField();
+		reci3.setToolTipText("");
+		reci3.setText("6");
+		reci3.setEditable(false);
+		reci3.setColumns(10);
+		reci3.setBounds(192, 460, 46, 32);
+		frame.getContentPane().add(reci3);
 		
-		textField_9 = new JTextField();
-		textField_9.setToolTipText("");
-		textField_9.setText("4");
-		textField_9.setEditable(false);
-		textField_9.setColumns(10);
-		textField_9.setBounds(242, 460, 46, 32);
-		frame.getContentPane().add(textField_9);
+		reci4 = new JTextField();
+		reci4.setToolTipText("");
+		reci4.setText("4");
+		reci4.setEditable(false);
+		reci4.setColumns(10);
+		reci4.setBounds(242, 460, 46, 32);
+		frame.getContentPane().add(reci4);
 		
-		textField_10 = new JTextField();
-		textField_10.setEnabled(false);
-		textField_10.setColumns(10);
-		textField_10.setBounds(292, 460, 105, 32);
-		frame.getContentPane().add(textField_10);
+		reci5 = new JTextField();
+		reci5.setEnabled(false);
+		reci5.setColumns(10);
+		reci5.setBounds(292, 460, 105, 32);
+		frame.getContentPane().add(reci5);
 		
-		textField_11 = new JTextField();
-		textField_11.setEnabled(false);
-		textField_11.setColumns(10);
-		textField_11.setBounds(399, 460, 179, 32);
-		frame.getContentPane().add(textField_11);
+		reci6 = new JTextField();
+		reci6.setEnabled(false);
+		reci6.setColumns(10);
+		reci6.setBounds(399, 460, 179, 32);
+		frame.getContentPane().add(reci6);
 		
-		textField_12 = new JTextField();
-		textField_12.setEnabled(false);
-		textField_12.setColumns(10);
-		textField_12.setBounds(582, 460, 118, 32);
-		frame.getContentPane().add(textField_12);
+		reci7 = new JTextField();
+		reci7.setEnabled(false);
+		reci7.setColumns(10);
+		reci7.setBounds(582, 460, 118, 32);
+		frame.getContentPane().add(reci7);
 		
-		textField_13 = new JTextField();
-		textField_13.setEditable(false);
-		textField_13.setColumns(10);
-		textField_13.setBounds(704, 460, 179, 32);
-		frame.getContentPane().add(textField_13);
+		reci8 = new JTextField();
+		reci8.setEditable(false);
+		reci8.setColumns(10);
+		reci8.setBounds(704, 460, 179, 32);
+		frame.getContentPane().add(reci8);
 		
-		textField_14 = new JTextField();
-		textField_14.setEditable(false);
-		textField_14.setColumns(10);
-		textField_14.setBounds(885, 460, 118, 32);
-		frame.getContentPane().add(textField_14);
+		reci9 = new JTextField();
+		reci9.setEditable(false);
+		reci9.setColumns(10);
+		reci9.setBounds(885, 460, 118, 32);
+		frame.getContentPane().add(reci9);
 		
 		JButton btnNewButton = new JButton("ENVIAR");
-		btnNewButton.setBounds(42, 53, 217, 161);
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+					progressBar43.setValue(progressBar43.getValue()+1);
+					
+			}
+		});
+		btnNewButton.setBounds(329, 81, 217, 161);
 		frame.getContentPane().add(btnNewButton);
+		
+		lblNewLabel = new JLabel("Enviar");
+		lblNewLabel.setBounds(21, 235, 92, 26);
+		frame.getContentPane().add(lblNewLabel);
+		
+		lblNewLabel_1 = new JLabel("Recivir");
+		lblNewLabel_1.setBounds(21, 391, 92, 26);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		lblOpercacion = new JLabel("Operacion");
+		lblOpercacion.setBounds(292, 277, 105, 26);
+		frame.getContentPane().add(lblOpercacion);
+		
+		lblHardware = new JLabel("Hardware \r\ntype");
+		lblHardware.setBounds(0, 274, 146, 32);
+		frame.getContentPane().add(lblHardware);
+		
+		label = new JLabel("Hardware \r\ntype");
+		label.setBounds(0, 427, 146, 32);
+		frame.getContentPane().add(label);
+		
+		label_1 = new JLabel("Operacion");
+		label_1.setBounds(292, 430, 105, 26);
+		frame.getContentPane().add(label_1);
+		
+		label_2 = new JLabel("MAC origen");
+		label_2.setBounds(418, 430, 152, 26);
+		frame.getContentPane().add(label_2);
+		
+		label_3 = new JLabel("IP origen");
+		label_3.setBounds(591, 430, 92, 26);
+		frame.getContentPane().add(label_3);
+		
+		label_4 = new JLabel("MAC destino");
+		label_4.setBounds(741, 430, 136, 26);
+		frame.getContentPane().add(label_4);
+		
+		label_5 = new JLabel("IP destino");
+		label_5.setBounds(885, 430, 105, 26);
+		frame.getContentPane().add(label_5);
+		
+		progressBar43 = new JProgressBar();
+	
+		progressBar43.setBounds(50, 361, 243, 14);
+		frame.getContentPane().add(progressBar43);
 	}
 }
